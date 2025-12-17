22db2620877ba3d4178d8203886df00d4ec7fda4
Cheewit <cheewit1996@gmail.com>
2025-12-17 14:00:51 +0700

    Delete components directory

--- components/AnalyticsDashboardScreen.tsx
+++
@@ -1,9 +0,0 @@
-// © 2025 Cheewit Manketwit. All rights reserved. BaristA:i_V4.3_CMK_CODE_SIG
-import React from 'react';
-
-const AnalyticsDashboardScreen: React.FC = () => {
-  // This component is currently a placeholder.
-  return null;
-};
-
-export default AnalyticsDashboardScreen;
--- components/FloatingVoiceButton.tsx
+++
@@ -1,2 +0,0 @@
-// © 2025 Cheewit Manketwit. All rights reserved.
-// This feature has been disabled as per user request to remove all voice-related functionality.
