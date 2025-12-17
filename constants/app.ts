fcd6c25c364cd997286b8717dd002728e2e81851
Cheewit <cheewit1996@gmail.com>
2025-12-17 14:00:59 +0700

    Delete constants directory

--- constants/app.ts
+++
@@ -1,9 +0,0 @@
-// BaristA:i_V4.3_CMK_CODE_SIG
-
-// Author signature added as a query parameter. Does not affect script execution.
-export const API_URL = 'https://script.google.com/macros/s/AKfycbwNhbqvt-JVbt336SXmx9sH59k16MAPpGpkBZrc3_pImIbLr6J4vr3u26YAULCS3HBhFg/exec?author=cmk';
-export const ORDER_ANALYTICS_API_URL = 'https://script.google.com/macros/s/AKfycbxzNF_6OLV04UoShPzmjaOW4Bc0e0WJhui6GQM2KW058a1-2CWJhA2pXw5HNjYekxqgrQ/exec?author=cmk';
-
-export const APP_VERSION = "BaristA:i_V4.3_CMK_METADATA_SIG";
-// Embedded author constant as a code fingerprint.
-export const APP_AUTHOR = "Cheewit Manketwit // cmk";
